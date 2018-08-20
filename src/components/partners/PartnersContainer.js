80d257d41b6dc37196e8f88fa753e255f02f9148
hectorbliss <contacto@fixter.org>
2018-08-19 21:11:09 -0500

    styles mod

--- src/components/partners/PartnersContainer.js
+++ src/components/partners/PartnersContainer.js
@@ -39,10 +39,12 @@ class PartnersContainer extends Component {
                 <div className="partners">
                     <h2>Clientes</h2>
                     <hr className="line_gris"/>
-                    <p className="quote">"Hay muchas fotografías que están llenas de vida pero son confusas y difíciles de recordar. Es la fuerza de una imagen lo que importa."
+                    <p className="quote">
+                        "Hay muchas fotografías que están llenas de vida pero son confusas y 
+                        difíciles de recordar. Es la fuerza de una imagen lo que importa."
                         <br/>
                         Brassai 1899-1984</p>
-                    <div style={{width:"80%", margin:"50px auto", display:"flex", flexWrap:"wrap", justifyContent:"center"}}>
+                    <div style={styles.cards}>
 
                         {partners.length ? null : <Spin />}
 
@@ -55,6 +57,16 @@ class PartnersContainer extends Component {
                 <Footer />
             </div>
         );
+    }
+}
+
+const styles = {
+    cards:{
+        width:"80%", 
+        margin:"50px auto", 
+        display:"flex", 
+        flexWrap:"wrap", 
+        justifyContent:"center"
     }
 }
 
